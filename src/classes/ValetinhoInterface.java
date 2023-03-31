232327001cadb8a4f4412baa0fe6a7cf336484b6
allansmithll <allanammancio2004@gmail.com>
2023-03-31 08:50:53 -0300

    Pequena atualização no ValetinhoInterface

--- src/classes/ValetinhoInterface.java
+++ src/classes/ValetinhoInterface.java
@@ -10,6 +10,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import java.swing.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
